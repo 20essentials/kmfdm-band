677e0dad3cb5366c263e3cdb5718d90747470d4c
DominicDev <s0l0estudi4r@gmail.com>
2025-11-09 22:09:10 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'KMFDM';
+export let nameSongs = [
+	"MEGALOMANIAC", /*1*/
+	"ANARCHY", /*2*/
+	"PROFESSIONAL KILLER", /*3*/
+	"FREE YOUR HATE", /*4*/
+	"READY TO BLOW", /*5*/
+	"STRAY BULLET", /*6*/
+	"POWER", /*7*/
+	"SON OF A GUN", /*8*/
+	"DOGMA", /*9*/
+	"WRATH", /*10*/
+	"BAIT & SWITCH", /*11*/
+	"STRUT", /*12*/
+	"ME & MY GUN", /*13*/
+	"INANE", /*14*/
+	"D.I.Y", /*15*/
+	"ADIOS", /*16*/
+	"SYCOPHANT", /*17*/
+	"DOWN OUT", /*18*/
+	"FREE YOUR HATE", /*19*/
+	"HAU RUCK", /*20*/
+]
